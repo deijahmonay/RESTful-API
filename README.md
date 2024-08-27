d95a8e75b99f4fb73d672b95993147480ed010d0
deijahmonay <deijah@shack15.com>
2024-08-26 20:15:37 -0700

    create calculator api, update readme for route

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 | HTTP Method | Endpoint | CRUD Operation | Route Name | Description|
 |--|--|--|--|--|
 | Get | /recipes | Read | index | Display list of recipes |
-| Get | /recipes/new | None | New | Show form to fill out new recipe card |
+| Get | /recipes/:new | None | New | Show form to fill out new recipe card |
 | POST | /recipes | Create | create | Add a new recipe to recipe book |
 | Get | /recipes/:recipesID | Read | show | Display a specific recipe |
 | Get | /recipes/:recipeID/edit | Read | edit | Show a form to edit existing recipe |
-| Put | /recipes/recipeID | Update | update | Update a specific recipe |
+| Put | /recipes/:recipeID | Update | update | Update a specific recipe |
 | DELETE | /recipes/:recipeID | Delete | delete | Remove a specific recipe from the list |
--- server.js
+++ server.js
@@ -2,9 +2,38 @@ const express = require("express")
 const app = express()
 
 const PORT = 4000;
+let total = 0;
+
+app.use(express.json())
+
+
+app.get("/calculator", (req, res) => {
+    res.status(200).json({total})
+})
+
+app.post("/calculator", (req, res) => {
+    const number = req.body.number
+    const operation = req.body.operation
+
+    if(operation === 'add'){
+        total += number
+    }else if(operation === 'subtract') {
+        total -= number
+    }else if (operation === 'multiply') {
+        total *= number
+    }else if(operation === 'divide') {
+        total /= number
+    }else{
+        return res.status(400).json({error: 'Invalid operation'})
+    }
+    res.status(200).json({total})
+})
+
+
 
 app.listen(PORT, () => {           
     console.log("Listening on port: 4000")
 })
 
 // ALSO CAN USE app.listen(4000, () => {console.log("Listen on port:4000")})
+
